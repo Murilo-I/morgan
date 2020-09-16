b7ef650b32af435a5e6b3dd0b431c6c335424e3b
Lorran-v <loorranvitor@hotmail.com>
2020-09-16 17:20:04 -0300

    2° dia de codificação

---
+++ Morgan.py
@@ -0,0 +1,26 @@
+import speech_recognition as sr
+
+def monitora_microfone():
+    microfone = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Aguardando o Comando: ")
+        audio = microfone.listen(source)
+
+
+    try:
+        print(microfone.recognize_google(audio,language='pt-BR'))
+    except sr.UnknownValueError:
+        print("Sphinx could not understand audio")
+    except sr.RequestError as e:
+        print("Sphinx error; {0}".format(e))
+
+
+    try:
+
+        print("Google Speech Recognition thinks you said " + microfone.recognize_google(audio))
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+monitora_microfone()
