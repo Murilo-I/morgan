875a21f73c33fef6264bc7d420de80d84c74b9c5
Murilo-| <murilotegani@gmail.com>
2020-10-08 17:34:59 -0300

    3° dia de codificação

--- Morgan.py
+++ Morgan.py
@@ -1,18 +1,34 @@
 import speech_recognition as sr
 
 
+# CONFIGURAÇÕES #
+
+hotword = 'morgan'
+
+
+# FUNÇÕES PRINCIPAIS #
+
 def monitora_audio():
     microfone = sr.Recognizer()
     with sr.Microphone() as source:
-        print("Aguardando Comando: ")
-        audio = microfone.listen(source)
+        while True:
+            print("Aguardando Comando: ")
+            audio = microfone.listen(source)
 
-    try:
-        print(microfone.recognize_google(audio, language="pt-BR"))
-    except sr.UnknownValueError:
-        print("Morgan could not understand audio")
-    except sr.RequestError as e:
-        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+            try:
+                # trigger -> PALAVRA CHAVE(morgan), esperando comando #
+                trigger = microfone.recognize_google(audio, language="pt-BR")
+                trigger = trigger.lower()
+
+                if hotword in trigger:
+                    print('Comando: ', trigger)
+                    # comandos a executar #
+                    break
+
+            except sr.UnknownValueError:
+                print("Morgan could not understand audio")
+            except sr.RequestError as e:
+                print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
 
 monitora_audio()
